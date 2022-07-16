75c55e1d0d52b17f9e07775b76a2c59ec2a9fbc4
kaoru-k <kaoru_kimura@outlook.com>
2022-07-17 03:03:02 +0900

    キャラ表示と画像保存機能実装

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Reference
 - [Vue.js v3とWebRTCでWebカメラを利用するコピペで使えるサンプル](https://qiita.com/n0bisuke/items/1818d02929a7351ae54d)
 - [[CSS] videoタグを縦横比は維持しながら全画面表示したい](https://migi.me/css/video-keep-aspect-fullscreen/)
+- [[HTML5] Canvasに画像を貼り付ける](https://blog.katsubemakito.net/html5/canvas-image)
+- [JavaScriptで画像とかを同期的に読み込む方法＆コード例](https://pisuke-code.com/js-load-image-synchronously/)
+- [JavaScriptでcanvasをファイルに保存する方法（IE対応も）](https://blog.ver001.com/canvas_save_file/)
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -13,17 +13,31 @@
         </ion-toolbar>
       </ion-header>
 
-      <ion-select placeholder="Select Camera" v-model="selectedVideoDevice" @ionChange="onChange">
-        <ion-select-option v-for="video in videoDevices" :key="video.index" :value="video.index">
-          {{ video.index }}: {{ video.text }}
-        </ion-select-option>
-      </ion-select>
+      <ion-item>
+        <ion-label>Device :</ion-label>
+        <ion-select placeholder="Select Camera" v-model="selectedVideoDevice" @ionChange="onChange">
+          <ion-select-option v-for="video in videoDevices" :key="video.index" :value="video.index">
+            {{ video.index }}: {{ video.text }}
+          </ion-select-option>
+        </ion-select>
+      </ion-item>
+      <ion-item>
+        <ion-label>x:</ion-label>
+        <ion-input v-model.number="charaPos.x"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label>y:</ion-label>
+        <ion-input v-model.number="charaPos.y"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label>Image URL</ion-label>
+        <ion-input v-model.number="charaUrl" @ionChange="loadCharaImage"></ion-input>
+      </ion-item>
 
-      <div class="video-wrapper">
-        <video muted="true" autoplay playsinline :srcObject.prop="stream" />
-      </div>
+      <canvas ref="canvas"></canvas>
+
       <ion-fab vertical="bottom" horizontal="center" slot="fixed">
-        <ion-fab-button color="light">
+        <ion-fab-button color="light" @click="shot">
           <ion-icon :icon="cameraOutline"></ion-icon>
         </ion-fab-button>
       </ion-fab>
@@ -42,7 +56,7 @@
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSelect, IonSelectOption, IonIcon } from "@ionic/vue";
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonSelect, IonSelectOption, IonIcon, IonFab, IonFabButton, IonItem, IonLabel, IonInput } from "@ionic/vue";
 import { cameraOutline, cameraReverseOutline, settingsSharp } from 'ionicons/icons';
 import { defineComponent } from "vue";
 
@@ -59,6 +73,11 @@ export default defineComponent({
     IonSelect,
     IonSelectOption,
     IonIcon,
+    IonFab,
+    IonFabButton,
+    IonItem,
+    IonLabel,
+    IonInput,
   },
   setup() {
     return {
@@ -69,9 +88,14 @@ export default defineComponent({
   },
   data() {
     return {
-      stream: {} as MediaStream,
+      chara: {} as HTMLImageElement,
+      charaPos: { x: 0, y: 0 },
+      charaUrl: 'https://4.bp.blogspot.com/-cD6qC3KnvzI/W6DTCD_LsII/AAAAAAABO4s/ObVOfI-_cTQPp7cyfPFiGdxr4cBU7jfjgCLcBGAs/s400/animal_stand_neko_white.png',
+      video: {} as HTMLVideoElement,
+      canvas: {} as HTMLCanvasElement,
+      offscreen: {} as HTMLCanvasElement,
       videoDevices: [] as VideoDevice[],
-      selectedVideoDevice: 0,
+      selectedVideoDevice: -1,
     };
   },
   mounted: async function () {
@@ -88,6 +112,22 @@ export default defineComponent({
           deviceId: video.deviceId,
         })
       );
+
+    this.video = document.createElement("video");
+    this.canvas = this.$refs.canvas as HTMLCanvasElement;
+    this.offscreen = document.createElement("canvas");
+
+    await this.loadCharaImage();
+
+    this.video.onloadedmetadata = () => {
+      this.video.play();
+
+      // Canvasのサイズを映像に合わせる
+      this.canvas.width = this.offscreen.width = this.video.videoWidth;
+      this.canvas.height = this.offscreen.height = this.video.videoHeight;
+
+      this.tick();
+    };
 
     if (this.videoDevices.length > 0) {
       this.selectedVideoDevice = 0;
@@ -112,7 +152,43 @@ export default defineComponent({
           : false,
       };
 
-      this.stream = await navigator.mediaDevices.getUserMedia(constraints);
+      this.video.srcObject = await navigator.mediaDevices.getUserMedia(constraints);
+    },
+    loadCharaImage: function () {
+      this.chara = new Image();
+      this.chara.crossOrigin = "Anonymous";
+      this.chara.src = this.charaUrl;
+    },
+    tick: async function () {
+      const offscreenCtx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+      // カメラの映像をCanvasに描画する
+      offscreenCtx.drawImage(this.video, 0, 0);
+      
+      // 画像を重ねる
+      offscreenCtx.drawImage(this.chara, this.charaPos.x, this.charaPos.y);
+
+      // イメージデータを取得する（[r,g,b,a,r,g,b,a,...]のように1次元配列で取得できる）
+      const imageData = offscreenCtx.getImageData(0, 0, this.offscreen.width, this.offscreen.height);
+
+      // オフスクリーンCanvasを更新する
+      offscreenCtx.putImageData(imageData, 0, 0);
+
+      // 表示用Canvasに描画する
+      const canvasCtx = this.canvas.getContext("2d") as CanvasRenderingContext2D;
+      canvasCtx.drawImage(this.offscreen, 0, 0);
+
+      // 次フレームを処理する
+      window.requestAnimationFrame(this.tick);
+    },
+    shot: async function () {
+      //アンカータグを作成
+      var a = document.createElement('a');
+      //canvasをJPEG変換し、そのBase64文字列をhrefへセット
+      a.href = this.canvas.toDataURL('image/jpeg', 0.85);
+      //ダウンロード時のファイル名を指定
+      a.download = 'download.jpg';
+      //クリックイベントを発生させる
+      a.click();
     },
   },
 });
@@ -132,8 +208,8 @@ export default defineComponent({
       transform: translateX(-50%) translateY(-50%);
       min-width: 100%;
       min-height: 100%;
+    }
   }
-}
 
   ion-fab.settings {
     ion-fab-button {
